637e3c51b958df2bb4c3eb4bb3e695d3c1c42390
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-02-12 04:14:59 -0800

    user repos accessed in the console

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // FORMAT THE GITHUB API URL
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    // MAKE THE SPECIFIC REQUEST TO THE URL AKA SERVER API
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+          console.log(data);
+        });
+    });
 };
+console.log("outside");
 
-getUserRepos();
+getUserRepos("ChristiLewis");
